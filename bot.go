5089f12b71f5d03eabbffb9afb04386e873d2fc5
SPdoesCode <lolsp.two@gmail.com>
2025-07-22 16:14:39 -0400

    janky ahh slash commands

--- bot.go
+++ bot.go
@@ -19,6 +19,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// config structs
+
 type channels struct {
 	Server  string `toml:"server"`
 	Channel string `toml:"channel"`
@@ -128,21 +130,22 @@ func ctrlMessages(self *discordgo.Session, message *discordgo.MessageCreate) {
 		case "eat":
 			fmt.Println("Command is hug with args as ", args, " sent by ", message.Author)
 			self.ChannelMessageSend(message.ChannelID, "<@"+message.Author.ID+"> ate "+strings.Join(args[1:], " ")+" kindly with love <3")
-			fmt.Println("Sent \"<@" + message.Author.ID + "> huged " + strings.Join(args[1:], " ") + " kindly...\"")
+			fmt.Println("Sent \"<@"+message.Author.ID+"> ate "+strings.Join(args[1:], " ")+" kindly with love <3\" to ", message.ChannelID)
 
 		case "hug":
 			fmt.Println("Command is eat with args as ", args, " sent by ", message.Author)
 			if rand.Intn(56+56*2) == 8 {
 				self.ChannelMessageSend(message.ChannelID, "<@"+message.Author.ID+"> huged "+strings.Join(args[1:], " ")+" kindly... then started to eat them...")
+				fmt.Println("Sent \"<@"+message.Author.ID+"> huged "+strings.Join(args[1:], " ")+" kindly... then started to eat them...\" to ", message.ChannelID)
 			} else {
 				self.ChannelMessageSend(message.ChannelID, "<@"+message.Author.ID+"> huged "+strings.Join(args[1:], " ")+" kindly... nothing else...")
+				fmt.Println("Sent \"<@"+message.Author.ID+"> huged "+strings.Join(args[1:], " ")+" kindly... nothing else...\" to ", message.ChannelID)
 			}
-			fmt.Println("Sent \"<@" + message.Author.ID + "> huged " + strings.Join(args[1:], " ") + " kindly...\"")
 
 		case "help":
 			fmt.Println("Command is help with args as ", args, " sent by ", message.Author)
 			self.ChannelMessageSend(message.ChannelID, "Commands: help, kill, sex, getvro, eat, hug, goonto")
-			fmt.Println("Sent help message")
+			fmt.Println("Sent help message to ", message.ChannelID)
 
 		default:
 			self.ChannelMessageSend(message.ChannelID, "Command "+cmd+" not found! Try the help command for more commands!")
@@ -177,6 +180,64 @@ func hourlyMessage(self *discordgo.Session) {
 	}()
 }
 
+// slash command handler
+func slashCmd(self *discordgo.Session, cmd *discordgo.InteractionCreate) {
+	var args string
+	options := cmd.ApplicationCommandData().Options
+	for _, opt := range options {
+		args = opt.StringValue()
+	}
+	switch cmd.ApplicationCommandData().Name {
+	case "getvro":
+		fmt.Println("Command is getvro with args as ", args, " sent by ", cmd.Member.User.Username)
+		fmt.Println("Sending sendRand() to channel ", cmd.ChannelID)
+		sendRand(self, cmd.ChannelID)
+	case "kill":
+		fmt.Println("Command is kill with args as ", args, " sent by ", cmd.Member.User.Username)
+		if len(config.Deaths) == 0 {
+			self.ChannelMessageSend(cmd.ChannelID, "No deaths configured.")
+			fmt.Println("WARN: No images in config.Deaths")
+			break
+		}
+		dnum := rand.Intn(len(config.Deaths))
+
+		self.ChannelMessageSend(cmd.ChannelID, "<@"+cmd.Member.User.ID+"> killed "+args+" with a "+config.Deaths[dnum])
+		fmt.Println("Sent: \"<@"+cmd.Member.User.ID+"> killed "+args+" with a "+config.Deaths[dnum]+"\" to ", cmd.ChannelID)
+	case "sex": // this is a joke command
+		fmt.Println("Command is sex with args as ", args, " sent by ", cmd.Member.User.Username)
+		if rand.Intn(2) == 0 {
+			self.ChannelMessageSend(cmd.ChannelID, "<@"+cmd.Member.User.ID+"> had sex with "+args+" and made them pregnant!")
+			fmt.Println("Sent: \"<@"+cmd.Member.User.ID+"> had sex with "+args+" and made them pregnant!\" to ", cmd.ChannelID)
+		} else {
+			self.ChannelMessageSend(cmd.ChannelID, "<@"+cmd.Member.User.ID+"> had sex with "+args+" and failed to make them pregnant!")
+			fmt.Println("Sent: \"<@"+cmd.Member.User.ID+"> had sex with "+args+" and failed to make them pregnant!\" to ", cmd.ChannelID)
+		}
+	case "hug":
+		fmt.Println("Command is eat with args as ", args, " sent by ", cmd.Member.User.Username)
+		if rand.Intn(56+56*2) == 8 {
+			self.ChannelMessageSend(cmd.ChannelID, "<@"+cmd.Member.User.ID+"> huged "+args+" kindly... then started to eat them...")
+			fmt.Println("Sent \"<@"+cmd.Member.User.ID+"> huged "+args+" kindly... then started to eat them...\" to ", cmd.ChannelID)
+		} else {
+			self.ChannelMessageSend(cmd.ChannelID, "<@"+cmd.Member.User.ID+"> huged "+args+" kindly... nothing else...")
+			fmt.Println("Sent \"<@"+cmd.Member.User.ID+"> huged "+args+" kindly... nothing else...\" to ", cmd.ChannelID)
+		}
+
+	case "goonto": // yet another obv joke command
+		fmt.Println("Command is goonto with args as ", args, " sent by ", cmd.Member.User.Username)
+		self.ChannelMessageSend(cmd.ChannelID, "<@"+cmd.Member.User.ID+"> gooned to "+args+"!!!!11!")
+
+	case "eat":
+		fmt.Println("Command is hug with args as ", args, " sent by ", cmd.Member.User.Username)
+		self.ChannelMessageSend(cmd.ChannelID, "<@"+cmd.Member.User.ID+"> ate "+args+" kindly with love <3")
+		fmt.Println("Sent \"<@"+cmd.Member.User.ID+"> huged "+args+" kindly with love <3\" to ", cmd.ChannelID)
+
+	case "help":
+		fmt.Println("Command is help with args as ", args, " sent by ", cmd.Member.User.ID)
+		self.ChannelMessageSend(cmd.ChannelID, "Commands: help, kill, sex, getvro, eat, hug, goonto")
+		fmt.Println("Sent help message to ", cmd.ChannelID)
+	}
+}
+
 func main() {
 
 	getConfig()
@@ -189,6 +250,81 @@ func main() {
 		os.Exit(1)
 	}
 
+	cmds := []*discordgo.ApplicationCommand{ // slash commands init
+		{
+			Name:        "getvro",
+			Description: "send a randome vro image",
+		},
+		{
+			Name:        "sex",
+			Description: "sex someone",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "thing",
+					Description: "the thing u wana sex",
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "eat",
+			Description: "eat someone",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "thing",
+					Description: "the thing u wana eat",
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "kill",
+			Description: "kill someone",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "thing",
+					Description: "the thing u wana kill",
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "goonto",
+			Description: "goon to a thing",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "thing",
+					Description: "the thing u wana goon to",
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "hug",
+			Description: "hug someone",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type:        discordgo.ApplicationCommandOptionString,
+					Name:        "thing",
+					Description: "the thing u wana hug",
+					Required:    true,
+				},
+			},
+		},
+	}
+
+	for _, cmd := range cmds {
+		_, err := bot.ApplicationCommandCreate(bot.State.User.ID, "", cmd)
+		if err != nil {
+			fmt.Println("ERROR: Couldnt register command ", cmd, ": ", err)
+		}
+	}
+
+	bot.AddHandler(slashCmd)     // slash commands
 	bot.AddHandler(ctrlMessages) // grab messages into the message control
 
 	bot.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsMessageContent // request perms
